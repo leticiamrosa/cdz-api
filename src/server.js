ca48f319a12200404a4118a29ff450bba18484d9
leticiamrosa <leticiamonteiro.design@gmail.com>
2019-08-11 07:50:55 -0300

    fix env

---
+++ src/server.js
@@ -0,0 +1,24 @@
+require("dotenv/config");
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const port = 3333;
+const server = express();
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+
+// mongoose.connect(
+//     "mongodb+srv://aryastark:got159@cluster0-7iq6q.mongodb.net/cdzApi?retryWrites=true&w=majority",
+//     { useNewUrlParser: true }
+//   );
+
+// this method 'use' its for add a file config in another file
+server.use(cors());
+server.use(express.json()); // this is for use json with express
+server.use(routes);
+
+server.listen(port, () => console.log(`Server running in localhost:${port}/`));
